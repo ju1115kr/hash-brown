ec991ad6933073da3f1a0e421f48216571a372c9
Joon <kimmj8409@gmail.com>
2018-07-02 22:37:50 +0900

    오타 수정
    오타 수정!!

--- server/app/api_1_0/transaction.py
+++ server/app/api_1_0/transaction.py
@@ -8,7 +8,7 @@ from web3 import Web3, HTTPProvider
 def transaction_test():
     web3 = Web3(Web3.HTTPProvider("http://35.234.3.182:8545"))
     web3.personal.unlockAccount(web3.eth.accounts[12], "12341234", 0)
-    web3miner.start(2)
+    web3.miner.start(2)
     time.sleep(5)
     web3.miner.stop()
     return web3.eth.accounts[12]
