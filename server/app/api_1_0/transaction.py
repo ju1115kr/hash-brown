7cfab35cbea276d94253917a6f1bdcb4b37c24ef
Myeongjoon <kimmj8409@gmail.com>
2018-07-05 02:28:34 +0900

    star 조회 함수

--- server/app/api_1_0/transaction.py
+++ server/app/api_1_0/transaction.py
@@ -6,15 +6,9 @@ from time import sleep
 from web3 import Web3, HTTPProvider
 from web3.contract import ConciseContract
 
-@api.route('/transaction/star/<int:targetID>/<int:likeCount>/', methods=['GET'])
-def transactionStar(targetID,likeCount):
+def getStarContract(address = None):
     web3 = Web3(Web3.HTTPProvider("http://35.234.3.182:8545"))
     web3.personal.unlockAccount(web3.eth.accounts[5], "12341234", 0)
-    #web3.miner.start(2)
-    #time.sleep(5)
-    #web3.miner.stop()
-    #var myContract = new web3.eth.Contract(myABI, myContractAddress);
-    
     
     
     abi = '[\
@@ -62,9 +56,25 @@ def transactionStar(targetID,likeCount):
     "type": "constructor"\
     }\
     ]'
+
+    bytecode = "60806040818152806101138339810160405280516020909101516000805460ff191660ff9384161761ff001916610100939092169290920217815560ca90819061004990396000f30060806040526004361060485763ffffffff7c01000000000000000000000000000000000000000000000000000000006000350416632ebcb6538114604d578063b3a21849146075575b600080fd5b348015605857600080fd5b50605f6087565b6040805160ff9092168252519081900360200190f35b348015608057600080fd5b50605f6090565b60005460ff1690565b600054610100900460ff16905600a165627a7a723058206d7e0a86624805f441c66b25adc4f1077dd64f1e294a5b0ed79ce8293e2339680029"
     # Instantiate and deploy contract
-    contract = web3.eth.contract(abi=abi, bytecode="60806040818152806101138339810160405280516020909101516000805460ff191660ff9384161761ff001916610100939092169290920217815560ca90819061004990396000f30060806040526004361060485763ffffffff7c01000000000000000000000000000000000000000000000000000000006000350416632ebcb6538114604d578063b3a21849146075575b600080fd5b348015605857600080fd5b50605f6087565b6040805160ff9092168252519081900360200190f35b348015608057600080fd5b50605f6090565b60005460ff1690565b600054610100900460ff16905600a165627a7a723058206d7e0a86624805f441c66b25adc4f1077dd64f1e294a5b0ed79ce8293e2339680029")
     
+    if address != None:
+        contract = web3.eth.contract(address = address,abi=abi, bytecode=bytecode)
+    else :
+        contract = web3.eth.contract(abi=abi, bytecode=bytecode)
+
+    return web3,contract
+
+@api.route('/transaction/star/<string:address>', methods=['GET'])
+def GetTransactionStar(address):
+    web3,contract = getStarContract(address)
+    return str(contract.call().getStars())
+
+@api.route('/transaction/star/<int:targetID>/<int:likeCount>/', methods=['GET'])
+def SettransactionStar(targetID,likeCount):
+    web3,contract = getStarContract()
     # Get transaction hash from deployed contract
     tx_hash = contract.deploy(args= [targetID,likeCount],transaction={'from': web3.eth.accounts[5], 'gas': 2900000})
     
